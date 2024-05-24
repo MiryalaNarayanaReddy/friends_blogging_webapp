75c7706a722f4f17e6a1c607fddc6a27d06cf258
Miryala Narayana Reddy <miryala.narayana@students.iiit.ac.in>
2024-05-24 19:59:03 +0530

    updated blogwrite page with adding para features

--- frontend/friendsblog/src/components/Wrapper.tsx
+++ frontend/friendsblog/src/components/Wrapper.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export function WindowCard({ children }: { children: React.ReactNode }): React.ReactNode {
     return (
         <div className="h-screen w-full bg-transparent  mt-20 mb-40 items-center justify-center flex ">
-          
-          {children}
+
+            {children}
 
         </div>
     )
@@ -14,7 +14,7 @@ export function WindowCard({ children }: { children: React.ReactNode }): React.R
 export function WrapperCard({ children }: { children: React.ReactNode }): React.ReactNode {
     return (
 
-        <div className="items-center justify-center h-screen w-4/5 bg-white shadow-lg rounded-lg  overflow-y-auto grid grid-cols-1 p-10">
+        <div className="h-screen w-4/5 bg-white shadow-lg rounded-lg  overflow-y-auto flex flex-col items-center">
 
             {children}
         </div>
@@ -24,20 +24,23 @@ export function WrapperCard({ children }: { children: React.ReactNode }): React.
 
 export function TitleCard({ children }: { children: React.ReactNode }): React.ReactNode {
     return (
-        <div className="text-2xl bg-transparent mt-4 rounded-lg p-4 w-full text-center p-1 col-span-1">
+        <div className="text-2xl bg-transparent mt-4 rounded-lg p-4 w-full text-center p-1  ">
 
             {children}
-    
+
         </div>
     )
 }
 
 
 
-export function ParagraphWrapperCard({ children }: { children: React.ReactNode }): React.ReactNode {
+export function ParagraphWrapperCard({ children}: { children: React.ReactNode}): React.ReactNode {
+
+    // detect to mouse press outside
+
     return (
 
-        <div className="cols-span-1">
+        <div className=" w-full flex flex-col justify-center">
 
             {children}
         </div>
@@ -46,29 +49,72 @@ export function ParagraphWrapperCard({ children }: { children: React.ReactNode }
 
 
 
-export function ParagraphCard({ children }: { children: React.ReactNode }): React.ReactNode {
+export function ParagraphCard({ children, id, addParagraph ,text, editParagraph}: { children: React.ReactNode, id: string, addParagraph: (id: string, position: number) => void, text: string, editParagraph: (text: string, id: string) => void }): React.ReactNode {
+
+    const [isHovered, setIsHovered] = React.useState(false);
+
     return (
-        <div className=" text-xl bg-transparent mt-4 rounded-lg p-4 w-full text-left p-1">
+        <div  key={id}
+        onMouseLeave={() => {
+            // e.currentTarget.style.backgroundColor = 'rgba(0,0,0,0)';
+            setIsHovered(false);
+        }}
+            onMouseEnter={() => {
+                // e.currentTarget.style.backgroundColor = 'rgba(0,0,0,0.1)';
+                setIsHovered(true);
+            }}  onDoubleClick={() => editParagraph(text, id)}>
 
-            {children}
-    
-        </div>
+            {/* hidden */}
+
+            <div className={`${isHovered ? '' : 'invisible'} flex justify-center z-10`}>
+                <button onClick={() => addParagraph(id, 0)}>
+                    + add new para above
+                </button>
+            </div>
+            <div className=" text-xl bg-transparent mt-4 rounded-lg p-4 w-full text-left p-1 ">
+
+                {children}
+
+            </div>
+
+            <div className={`${isHovered ? '' : 'invisible'} flex justify-center z-10`} >
+                <button className="shadow-lg" onClick={() => addParagraph(id, 1)}>
+                    + add new para below
+                </button>
+            </div>
+        </div >
     )
 }
 
-export function ParagraphEditCard({ children }: { children: React.ReactNode }): React.ReactNode {
+export function ParagraphEditCard({ children , id ,addParagraph}: { children: React.ReactNode, id: string, addParagraph: (id: string, position: number) => void }): React.ReactNode {
+
     return (
-        <div className=" text-xl text-center  flex flex-col  justify-center bg-transparent mt-4 rounded-lg p-4 w-full shadow-2xl ">
+        <div>
 
-            {children}
-    
+
+            <div className="flex justify-center z-10">
+                <button onClick={() => addParagraph(id, 0)}>
+                    + add new para above
+                </button>
+            </div>
+            <div className=" text-xl text-center  flex flex-col  justify-center bg-transparent mt-4 rounded-lg p-4 w-full shadow-2xl ">
+
+                {children}
+
+            </div>
+
+            <div className="flex justify-center z-10">
+                <button className="shadow-lg" onClick={() => addParagraph(id, 1)}>
+                    + add new para below
+                </button>
+            </div>
         </div>
     )
 }
 
 
 
-export function InputTitleCard(props: { label: string,placeholder:string, name: string, type: string, value: string, setValue: React.SetStateAction<any> }): React.ReactNode {
+export function InputTitleCard(props: { label: string, placeholder: string, name: string, type: string, value: string, setValue: React.SetStateAction<any> }): React.ReactNode {
     return (
         <div className="mb-4">
 
--- frontend/friendsblog/src/pages/blog/WriteBlog.tsx
+++ frontend/friendsblog/src/pages/blog/WriteBlog.tsx
@@ -1,70 +1,140 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useState } from "react";
 import { ParagraphCard, ParagraphWrapperCard, WindowCard, WrapperCard } from "../../components/Wrapper";
 import { ParagraphEditCard } from "../../components/Wrapper";
 import { TitleCard } from "../../components/Wrapper";
 import { InputTitleCard } from "../../components/Wrapper";
 
-// create large one
-const paragraph = [
-    'in the beginning god created the heavens and the earth. the earth was without form and void, and darkness was over the face of the deep. and the spirit of god was hovering over the face of the waters. and god said, “let there be light,” and there was light. and god saw that the light was good. and god separated the light from the darkness. god called the light day, and the darkness he called night. and there was evening and there was morning, the first day.',
-    'and god said, “let there be an expanse in the midst of the waters, and let it separate the waters from the waters.” and god made the expanse and separated the waters that were under the expanse from the waters that were above the expanse. and it was so. and god called the expanse heaven. and there was evening and there was morning, the second day.',
-    'and god said, “let the waters under the heavens be gathered together into one place, and let the dry land appear.” and it was so. god called the dry land earth, and the waters that were gathered together he called seas. and god saw that it was good. and god said, “let the earth sprout vegetation, plants yielding seed, and fruit trees bearing fruit in which is their seed, each according to its kind, on the earth.” and it was so. the earth brought forth vegetation, plants yielding seed according to their own kinds, and trees bearing fruit in which is their seed, each according to its kind. and god saw that it was good. and there was evening and there was morning, the third day.',
-    'and god said, “let there be lights in the expanse of the heavens to separate the day from the night. and let them be for signs and for seasons, and for days and years, and let them be lights in the expanse of the heavens to give light upon the earth.” and it was so. and god made the two great lights—the greater light to rule the day and the lesser light to rule the night—and the stars. and god set them in the expanse of the heavens to give light on the earth, to rule over the day and over the night, and to separate the light from the darkness. and god saw that it was good. and there was evening and there was morning, the fourth day.',
-    'and god said, “let the waters swarm with swarms of living creatures, and let birds fly above the earth across the expanse of the heavens.” so god created the great sea creatures and every living creature that moves, with which the waters swarm, according to their kinds, and every winged bird according to its kind. and god saw that it was good. and god blessed them, saying, “be fruitful and multiply and fill the waters in the seas, and let birds multiply on the earth.” and there was evening and there was morning, the fifth day.',
-    'and god said, “let the earth bring forth living creatures according to their kinds—livestock and creeping things and beasts of the earth according to their kinds.” and it was so. and god made the beasts of the earth according to their kinds and the livestock according to their kinds, and everything that creeps on the ground according to its kind. and god saw that it was good.'
-]
 
-function WriteBlog(): React.ReactNode {
-    const [title, setTitle] = useState('');
-    const [paragraphs, setParagraphs] = useState<{ [id: string]: string }>({});
-    const [currentParagraph, setCurrentParagraph] = useState<string>('');
-    const [currentParagraphIndex, setCurrentParagraphIndex] = useState<string>('1');
 
-    useEffect(() => {
-        // set the paragraph dict with the paragraph array
-        let p: { [id: string]: string } = {};
-        paragraph.forEach((text, i) => {
-            p[i.toString()] = text;
-        });
-        setParagraphs(p);
-    }, []);
+function renderParagraphCard(text: string, id: string, currentParagraphIndex: string, setCurrentParagraph: (text: string) => void, setCurrentParagraphIndex: (id: string) => void, saveParagraph: (id: string) => void, addParagraph: (id: string, position: number) => void): React.ReactNode {
 
-    function handleParagraphChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
-        setCurrentParagraph(e.target.value);
-    }
+    const editParagraphCard = (text: string, id: string) => {
 
-    function saveParagraph(id: string) {
-        let p = { ...paragraphs };
-        p[id] = currentParagraph;
-        setParagraphs(p);
-        setCurrentParagraph('');
-        setCurrentParagraphIndex('1');
-    }
+        // if the current paragraph is not empty save it
 
-    function renderParagraphCard(text: string, id: string): React.ReactNode {
-        return (
-            <ParagraphCard key={id} >
-                <p onClick={() => editParagraphCard(text, id)}>{text}</p>
+        saveParagraph(currentParagraphIndex);
 
-            </ParagraphCard>
-        );
-    }
-
-    function editParagraphCard(text: string, id: string) {
         setCurrentParagraph(text);
         setCurrentParagraphIndex(id);
     }
 
+    return (
+        <ParagraphCard id={id} text={text} addParagraph={addParagraph} editParagraph={editParagraphCard} >
+            <p key={id}>{text}</p>
+        </ParagraphCard>
+    );
+}
 
-    // if currsor click below the last paragraph add a new paragraph
+function renderEditParagraphCard(id: string, currentParagraph: string, setCurrentParagraph: (text: string) => void, saveParagraph: (id: string) => void, addParagraph: (id: string, position: number) => void): React.ReactNode {
+
+    const handleParagraphChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setCurrentParagraph(e.target.value);
+    }
+
+    // calculate height of textarea
+    // width = 4/5 of the parent
+
+
+    // const height:string = currentParagraph.split(' ').length * 1.5 + 'rem';
+
+    // use scroll height of text area
+    const height = document.getElementById('textarea')?.scrollHeight;
+    // set width to 4/5 of the parent
+
+    return (
+        <ParagraphEditCard id={id} addParagraph={addParagraph} >
+            <textarea key={id}
+                id="textarea"
+                className="w-full overflow-y-auto p-4"
+                style={{ height: height }}
+                value={currentParagraph}
+                onChange={handleParagraphChange}
+            ></textarea>
+
+            <button onClick={() => saveParagraph(id)}>Save</button>
+        </ParagraphEditCard>
+    )
+}
+
+
+function WriteBlog(): React.ReactNode {
+    const [title, setTitle] = useState('');
+    const [paragraphs, setParagraphs] = useState<{ [id: string]: string }>({});
+
+    const [currentParagraph, setCurrentParagraph] = useState<string>('');
+    const [currentParagraphIndex, setCurrentParagraphIndex] = useState<string>('');
+
+    const saveParagraph = (id: string) => {
+
+        if (currentParagraphIndex === '-1') {
+            return;
+        }
+
+        if (currentParagraph === '') {
+
+            let p = { ...paragraphs };
+            delete p[id];
+            setParagraphs(p);
+            setCurrentParagraph('');
+            setCurrentParagraphIndex('-1');
+            return;
+
+
+        }
+
+        // let p = { ...paragraphs };
+        // p[id] = currentParagraph;
+        setParagraphs({ ...paragraphs, [id]: currentParagraph });
+        setCurrentParagraph('');
+        setCurrentParagraphIndex('-1');
+    }
+
+
+    const addParagraph = (id: string, position: number) => {
+
+        // await saveParagraph(currentParagraphIndex);
+
+        // if (currentParagraphIndex !== '-1') {
+        //     saveParagraph(currentParagraphIndex);
+        // }
+
+        console.log('addParagraph', id, position);
+        console.log('currentParagraphIndex', currentParagraphIndex);
+        console.log('currentParagraph', currentParagraph);
+        console.log('paragraphs', paragraphs);
+
+
+        let p = Object.values(paragraphs);
+        const pos = parseInt(id);
+
+        // if (position === 0) {
+        //     p.splice(pos, 0, '');
+        // } else {
+        //     p.splice(pos + 1, 0, '');
+        // }
+
+        p.splice(position === 0 ? pos : pos + 1, 0, '');
+
+        let newP: { [id: string]: string } = {};
+        p.forEach((text, i) => {
+            newP[i.toString()] = text;
+        });
+
+        setParagraphs(newP);
+        setCurrentParagraphIndex((pos + position).toString());
+        setCurrentParagraph('');
+
+        console.log('newP', newP);
+        console.log('currentParagraphIndex', currentParagraphIndex);
+        console.log('currentParagraph', currentParagraph);
+        console.log('paragraphs', paragraphs);
+    }
 
 
     return (
         <WindowCard>
             <WrapperCard>
-
-
 
                 <TitleCard>
                     <div className="justify-center m-2  col-span-1">
@@ -75,45 +145,28 @@ function WriteBlog(): React.ReactNode {
 
 
                         {/* <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />*/}
-
-
                     </div>
 
+                   
+
                 </TitleCard>
-
-
-                <ParagraphWrapperCard>
-
-
+                <div className="justify-center z-10 text-sm shadow-lg w-40 m-4">
+                        <button onClick={() => addParagraph('0', 0)}>
+                            + add new para below
+                        </button>
+                    </div>
+                    
+                {/* // mouse press outside detect */}
+                <ParagraphWrapperCard  >
+                
 
                     {Object.keys(paragraphs).map((id) => {
                         if (id === currentParagraphIndex) {
-                            // calculate height of textarea
-                            // width = 4/5 of the parent
 
+                            return renderEditParagraphCard(id, currentParagraph, setCurrentParagraph, saveParagraph, addParagraph)
 
-                            // const height:string = currentParagraph.split(' ').length * 1.5 + 'rem';
-
-                            // use scroll height of text area
-                            const height = document.getElementById('textarea')?.scrollHeight;
-                            // set width to 4/5 of the parent
-
-
-                            return (
-                                <ParagraphEditCard key={id}>
-                                    <textarea
-                                        id="textarea"
-                                        className="w-full overflow-y-auto p-4"
-                                        style={{ height: height }}
-                                        value={currentParagraph}
-                                        onChange={handleParagraphChange}
-                                    ></textarea>
-
-                                    <button onClick={() => saveParagraph(id)}>Save</button>
-                                </ParagraphEditCard>
-                            );
                         } else {
-                            return renderParagraphCard(paragraphs[id], id);
+                            return renderParagraphCard(paragraphs[id], id, currentParagraphIndex, setCurrentParagraph, setCurrentParagraphIndex, saveParagraph, addParagraph);
                         }
                     })}
                 </ParagraphWrapperCard>
